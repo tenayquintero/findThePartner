3349af382bd08e67268fe96200a818adf1a44d56
Yaneth Quintero <quinteroftv@hotmail.com>
2022-06-20 14:12:58 +0100

    codigo optimizado y añadido el sonido de las cartas

--- js/findPartner.js
+++ js/findPartner.js
@@ -2,70 +2,101 @@
 const cards = document.querySelectorAll(".card");
 const button = document.querySelector(".center");
 const p = document.querySelector("p");
+const audio = new Audio("../sounds/barajar.mp3");
+
 
 let isTheCardFlipped = false;
 let firstCard, secondCard;
-let count=1;
+let count = 1;
+
+/**
+ * ########################
+ * ##Barajamos las cartas##
+ * ########################
+ */
+(function mix() {
+  cards.forEach((card) => {
+    let posicionRandom = Math.floor(Math.random() * 3);
+    card.style.order = posicionRandom;
+  });
+})();
+
+/*
+###############################################
+##Sacamos el tablero para comenzar la partida##
+###############################################
+*/
 
 const showGame = () => {
   button.style.display = "none";
-  p.style.display="block"
+  p.style.display = "block";
   let num = 1;
   let drawCards = setInterval(() => {
     if (num === 17) {
       clearInterval(drawCards);
     } else {
+      audio.play();
       document.querySelector(".card" + num++).style.display = "flex";
+      
     }
-  }, 50);
-
-  // p.style.display = "block";
-
-  // cards.forEach((element) => {
-  //   element.style.display = "flex";
-  // });
+  }, 80);
 };
 
 button.addEventListener("click", () => {
   showGame();
 });
 
+/*
+#################################################################
+##giramos las cartas e incrementamos movimientos cada dos click##
+#################################################################
+*/
+
 const reveal = (e) => {
   const currentCard = e.currentTarget;
   currentCard.classList.add("flipped");
 
+  //Hacemos el primer click
   if (!isTheCardFlipped) {
     isTheCardFlipped = true;
     firstCard = currentCard;
-
-    console.log("soy", firstCard.dataset.emoji);
-  } else {
-    isTheCardFlipped = false;
-    secondCard = currentCard;
-    p.textContent = "Movimientos " + count++
-
-    console.log("soy", secondCard.dataset.emoji);
-    if (firstCard.dataset.emoji === secondCard.dataset.emoji) {
-      firstCard.removeEventListener("click", reveal);
-      secondCard.removeEventListener("click", reveal);
-      console.log(" somos iguales");
-    } else {
-      setTimeout(() => {
-        firstCard.classList.remove("flipped");
-        secondCard.classList.remove("flipped");
-      }, 1000);
-    }
+    return;
   }
+  //Hacemos el segundo click
+  isTheCardFlipped = false;
+  secondCard = currentCard;
+  p.textContent = "Movimientos " + count++;
+
+  compareCards();
 };
 
+/**###############################################
+ * ##comparamos con operador ternario las cartas##
+ * ###############################################
+ */
+function compareCards() {
+  firstCard.dataset.emoji === secondCard.dataset.emoji
+    ? removeReveal() 
+    : removeFlipped();
+   
+}
+/**
+ * eliminamos la funcion click
+ */
+function removeReveal() {
+  firstCard.removeEventListener("click", reveal);
+  secondCard.removeEventListener("click", reveal);
+  
+}
+/**
+ * eliminamos el giro de la carta
+ */
+function removeFlipped() {
+  setTimeout(() => {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+  }, 1000);
+}
 for (const card of cards) {
   card.addEventListener("click", reveal);
 }
-
-//barajar cartas
-(function mezclar() {
-  cards.forEach((card) => {
-    let posicionRandom = Math.floor(Math.random() * 3);
-    card.style.order = posicionRandom;
-  });
-})();
