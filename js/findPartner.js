68ac73afffadc857acb1f49a6792d44cc203bdf7
Yaneth Quintero <quinteroftv@hotmail.com>
2022-06-20 11:14:30 +0100

    le hemos puesto logica al proyecto

--- js/findPartner.js
+++ js/findPartner.js
@@ -3,13 +3,27 @@ const cards = document.querySelectorAll(".card");
 const button = document.querySelector(".center");
 const p = document.querySelector("p");
 
+let isTheCardFlipped = false;
+let firstCard, secondCard;
+let count=1;
+
 const showGame = () => {
   button.style.display = "none";
-  p.style.display= "block"
+  p.style.display="block"
+  let num = 1;
+  let drawCards = setInterval(() => {
+    if (num === 17) {
+      clearInterval(drawCards);
+    } else {
+      document.querySelector(".card" + num++).style.display = "flex";
+    }
+  }, 50);
 
-  cards.forEach((element) => {
-    element.style.display = "flex";
-  });
+  // p.style.display = "block";
+
+  // cards.forEach((element) => {
+  //   element.style.display = "flex";
+  // });
 };
 
 button.addEventListener("click", () => {
@@ -20,9 +34,28 @@ const reveal = (e) => {
   const currentCard = e.currentTarget;
   currentCard.classList.add("flipped");
 
-  setTimeout(() => {
-    currentCard.classList.remove("flipped");
-  }, 1000);
+  if (!isTheCardFlipped) {
+    isTheCardFlipped = true;
+    firstCard = currentCard;
+
+    console.log("soy", firstCard.dataset.emoji);
+  } else {
+    isTheCardFlipped = false;
+    secondCard = currentCard;
+    p.textContent = "Movimientos " + count++
+
+    console.log("soy", secondCard.dataset.emoji);
+    if (firstCard.dataset.emoji === secondCard.dataset.emoji) {
+      firstCard.removeEventListener("click", reveal);
+      secondCard.removeEventListener("click", reveal);
+      console.log(" somos iguales");
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+      }, 1000);
+    }
+  }
 };
 
 for (const card of cards) {
